20e0e40510fd66bc9052535bfa4470644fa5dfee
João Inez <joaoinez.info@gmail.com>
2024-03-19 00:38:38 +0100

    PIC-7 - Bootstrap library

---
+++ lib/components/Box/index.tsx
@@ -0,0 +1,5 @@
+import styles from "./styles.module.css";
+
+export function Box() {
+  return <div className={styles.Box}></div>;
+}
---
+++ lib/components/Button/index.tsx
@@ -0,0 +1,5 @@
+import styles from "./styles.module.css";
+
+export function Button() {
+  return <button className={styles.Button}></button>;
+}
